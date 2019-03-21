1f0518eb7b32a7a6889f561c689ef2f515cfc33b
mitsuyoshi <m123matsuo@icloud.com>
2019-03-22 08:46:48 +0900

    Building LoginVC's User Interface

---
+++ htchhkr-development/Controller/LoginVC.swift
@@ -0,0 +1,30 @@
+//
+//  LoginVC.swift
+//  htchhkr-development
+//
+//  Created by mitsuyoshi matsuo on 2019/03/22.
+//  Copyright © 2019 mitsuyoshi matsuo. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
