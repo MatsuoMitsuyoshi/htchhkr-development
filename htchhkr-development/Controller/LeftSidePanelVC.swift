a825d5d118a83524a084c83d652f3ce813b7c36e
mitsuyoshi <m123matsuo@icloud.com>
2019-03-23 17:45:30 +0900

    Configuring Side Menu Changes Based On Account Type/incomplete

--- htchhkr-development/Controller/LeftSidePanelVC.swift
+++ htchhkr-development/Controller/LeftSidePanelVC.swift
@@ -7,18 +7,90 @@
 //
 
 import UIKit
+import Firebase
 
 class LeftSidePanelVC: UIViewController {
 
+    @IBOutlet weak var pickupModeSwitch: UISwitch!
+    @IBOutlet weak var pickupModeLbl: UILabel!
+    @IBOutlet weak var userImageView: RoundImageView!
+    @IBOutlet weak var userEmailLbl: UILabel!
+    @IBOutlet weak var userAccountTypeLbl: UILabel!
+    @IBOutlet weak var loginOutBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    @IBAction func signUpLoginBtnWasPressed(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginVC") as? LoginVC
-        present(loginVC!, animated: true, completion: nil)
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        pickupModeSwitch.isOn = false
+        pickupModeSwitch.isHidden = true
+        pickupModeLbl.isHidden = true
+        
+        observePassengersAndDrivers()
+        
+        if Auth.auth().currentUser == nil {
+            userEmailLbl.text = ""
+            userAccountTypeLbl.text = ""
+            userImageView.isHidden = true
+            loginOutBtn.setTitle("Sign Up / Login", for: .normal)
+        } else {
+            userEmailLbl.text = Auth.auth().currentUser?.email
+            userAccountTypeLbl.text = ""
+            userImageView.isHidden = false
+            loginOutBtn.setTitle("Logout", for: .normal)
+        }
     }
     
+    func observePassengersAndDrivers() {
+        DataService.instance.REF_USERS.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    if snap.key == Auth.auth().currentUser?.uid {
+                        self.userAccountTypeLbl.text = "PASSENGER"
+                    }
+                }
+            }
+        })
+        
+        DataService.instance.REF_DRIVERS.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    if snap.key == Auth.auth().currentUser?.uid {
+                        self.userAccountTypeLbl.text = "DRIVER"
+                        self.pickupModeSwitch.isHidden = false
+                        
+                        let switchStatus = snap.childSnapshot(forPath: "isPickupModeEnabled").value as! Bool
+                        self.pickupModeSwitch.isOn = switchStatus
+                        self.pickupModeLbl.isHidden = false
+                    }
+                }
+            }
+        })
+    }
+    
+
+    @IBAction func signUpLoginBtnWasPressed(_ sender: Any) {
+        if Auth.auth().currentUser == nil {
+            let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+            let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginVC") as? LoginVC
+            present(loginVC!, animated: true, completion: nil)
+        } else {
+            do {
+                try Auth.auth().signOut()
+                userEmailLbl.text = ""
+                userAccountTypeLbl.text = ""
+                userImageView.isHidden = true
+                pickupModeLbl.text = ""
+                pickupModeSwitch.isHidden = true
+                loginOutBtn.setTitle("Sign Up / Login", for: .normal)
+            } catch (let error) {
+                print(error)
+            }
+        }
+    }
 }
