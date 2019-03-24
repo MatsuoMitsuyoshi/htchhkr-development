da85fa5b03f107d6f8c80b0ceb505a1bfbd5e75f
mitsuyoshi <m123matsuo@icloud.com>
2019-03-25 02:30:12 +0900

    Loading Instances of Driver from Firebase

--- htchhkr-development/Controller/HomeVC.swift
+++ htchhkr-development/Controller/HomeVC.swift
@@ -10,6 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 import RevealingSplashView
+import Firebase
 
 class HomeVC: UIViewController {
 
@@ -37,6 +38,16 @@ class HomeVC: UIViewController {
         
         centerMapOnUserLocation()
         
+        DataService.instance.REF_DRIVERS.observe(.value, with: { (snapshot) in
+            self.loadDriverAnnotationsFromFB()
+            
+//            DataService.instance.passengerIsOnTrip(passengerKey: self.currentUserId!, handler: { (isOnTrip, driverKey, tripKey) in
+//                if isOnTrip == true {
+//                    self.zoom(toFitAnnotationsFromMapView: self.mapView, forActiveTripWithDriver: true, withKey: driverKey)
+//                }
+//            })
+        })
+        
         self.view.addSubview(revealingSplashView)
         revealingSplashView.animationType = SplashAnimationType.heartBeat
         revealingSplashView.startAnimation()
@@ -50,6 +61,52 @@ class HomeVC: UIViewController {
         } else {
             manager?.requestAlwaysAuthorization()
         }
+    }
+    
+    func loadDriverAnnotationsFromFB() {
+        DataService.instance.REF_DRIVERS.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let driverSnapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for driver in driverSnapshot {
+                    if driver.hasChild("coordinate") {
+                        if driver.childSnapshot(forPath: "isPickupModeEnabled").value as? Bool == true {
+                            if let driverDict = driver.value as? Dictionary<String, AnyObject> {
+                                let coordinateArray = driverDict["coordinate"] as! NSArray
+                                let driverCoordinate = CLLocationCoordinate2D(latitude: coordinateArray[0] as! CLLocationDegrees, longitude: coordinateArray[1] as! CLLocationDegrees)
+                                
+                                let annotation = DriverAnnotation(coordinate: driverCoordinate, withKey: driver.key)
+                                
+                                var driverIsVisible: Bool {
+                                    return self.mapView.annotations.contains(where: { (annotation) -> Bool in
+                                        if let driverAnnotation = annotation as? DriverAnnotation {
+                                            if driverAnnotation.key == driver.key {
+                                                driverAnnotation.update(annotationPosition: driverAnnotation, withCoordinate: driverCoordinate)
+                                                return true
+                                            }
+                                        }
+                                        return false
+                                    })
+                                }
+
+                                if !driverIsVisible {
+                                    self.mapView.addAnnotation(annotation)
+                                }
+                            }
+                        } else {
+                            for annotation in self.mapView.annotations {
+                                if annotation.isKind(of: DriverAnnotation.self) {
+                                    if let annotation = annotation as? DriverAnnotation {
+                                        if annotation.key == driver.key {
+                                            self.mapView.removeAnnotation(annotation)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        })
+//        revealingSplashView.heartAttack = true
     }
     
     func centerMapOnUserLocation() {
